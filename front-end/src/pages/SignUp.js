7c3fc0a9711ffbcbb906c62c01d39792cf5d7ccb
tung-td <tranduytung33@gmail.com>
2024-04-17 10:26:51 +0700

    Code new function
    Header, Signup, Tripcreate

--- front-end/src/pages/SignUp.js
+++ front-end/src/pages/SignUp.js
@@ -1,180 +1,225 @@
-import React, { useState } from 'react'
-import boy from '../assets/img/register.png'
-import girl from '../assets/img/register2.png'
-import { FcGoogle } from 'react-icons/fc'
-import { BsFacebook } from 'react-icons/bs'
-import {BiShow, BiHide} from 'react-icons/bi'
-import { Link, useNavigate } from 'react-router-dom'
-import {toast} from 'react-hot-toast'
-
-
+import React, { useState } from "react";
+import { BiShow, BiHide } from "react-icons/bi";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
+import banner1 from "../assets/Auth/login_banner_1.png";
+import { FaGoogle, FaApple, FaLongArrowAltLeft } from "react-icons/fa";
 
 export const SignUp = () => {
-  const navigate = useNavigate()
-  const API = process.env.REACT_APP_SERVER_DOMAIN
-  const [showPassword, setShowPassword] = useState(false)
-  const [showConfirmPassword, setConfirmPassword] = useState(false)
-  const [data, setData ]= useState({
+  const navigate = useNavigate();
+  const API = process.env.REACT_APP_SERVER_DOMAIN;
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setConfirmPassword] = useState(false);
+  const [data, setData] = useState({
     first_name: "",
     last_name: "",
     email: "",
     username: "",
     password: "",
-    confirmPassword: ""
-  })
+    confirmPassword: "",
+  });
 
   const handleOnchange = (e) => {
-    const {name, value} = e.target
-    setData((prev)=>{
-      return{
+    const { name, value } = e.target;
+    setData((prev) => {
+      return {
         ...prev,
-        [name]:value
-      }
-    })
-    console.log(data)
-  }
+        [name]: value,
+      };
+    });
+    console.log(data);
+  };
 
-  const handleSubmit = async(e) => {
-    e.preventDefault()
-    const {first_name, last_name, email, password, confirmPassword} = data
-    if(first_name && last_name && email && password && confirmPassword){
-      if(password === confirmPassword){
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { first_name, last_name, email, password, confirmPassword } = data;
+    if (first_name && last_name && email && password && confirmPassword) {
+      if (password === confirmPassword) {
         const fetchData = await fetch(`${API}signup/`, {
           method: "POST",
           headers: {
-            "Content-Type" : "application/json"
+            "Content-Type": "application/json",
           },
-          body: JSON.stringify(data)
-        })
-        const dataRes = await fetchData.json()
-        if(dataRes.message)
-          toast.success(dataRes.message)
-        else if  (dataRes.username)
-          toast.error(dataRes.username[0])
-        else
-          toast.error(dataRes.error )
-        console.log(dataRes)
-        if(dataRes.status)
-        {
-          navigate('/login')
+          body: JSON.stringify(data),
+        });
+        const dataRes = await fetchData.json();
+        if (dataRes.message) toast.success(dataRes.message);
+        else if (dataRes.username) toast.error(dataRes.username[0]);
+        else toast.error(dataRes.error);
+        console.log(dataRes);
+        if (dataRes.status) {
+          navigate("/login");
         }
+      } else {
+        toast.error("Password does not match");
       }
-      else{
-        toast.error("Password does not match")
-      }
+    } else {
+      toast.error("Please enter require field");
     }
-    else{
-      toast.error("Please enter require field")
-    }
-  }
-
-
+  };
 
   const handleShowPassword = () => {
-    setShowPassword(prev => !prev)
-  }
+    setShowPassword((prev) => !prev);
+  };
   const handleConfirmShowPassword = () => {
-    setConfirmPassword(prev => !prev)
-  }
-  return (
-    <div className=' flex mt-20 flex-1 ml-10 mr-10 max-h-screen'>
-      <div className=' flex-1 flex items-center justify-center relative'>
-        <img src={boy} alt='Hoi An' className=' object-cover h-2/4'/>
-        <img src={girl} alt='Hoi An' className=' object-cover h-2/4'/>
-        <p className=' absolute top-0 uppercase font-bold text-6xl text-indigo-500'>start your journey by one click, explore beautiful world</p>
-      </div>
-      <div className=' ml-10 flex-1 flex flex-col items-center '>
-        <h1 className=' text-4xl font-bold item text-slate-700'>Register</h1>
-        <form className='w-full py-3 flex flex-col mt-5' onSubmit={handleSubmit}>
-          <div className='flex'>
-            <div className=' flex-1'>
-              <label className=' font-medium text-slate-700'>First Name</label>
-              <input
-                type='text'
-                id='first_name'
-                name='first_name'
-                placeholder='Name'
-                value={data.first_name}
-                onChange={handleOnchange}
-                className='mt-1 mb-5 w-full border border-slate-300 px-2 py-2 rounded focus-within:outline-blue-300'
-              />
-            </div>
-            <div className='ml-5 flex-1'>
-              <label className=' font-medium text-slate-700'>Last Name</label>
-              <input
-                type='text'
-                id='last_name'
-                name='last_name'
-                placeholder='Name'
-                value={data.last_name}
-                onChange={handleOnchange}
-                className='mt-1 mb-2 w-full border border-slate-300 px-2 py-2 rounded focus-within:outline-blue-300'
-              />
-            </div>
-          </div>
-          {/* User name */}
-          <label className=' font-medium text-slate-700'>User Name</label>
-          <input
-            type='text'
-            id='username'
-            name='username'
-            value={data.username}
-            onChange={handleOnchange}
-            placeholder='User name'
-            className='mt-1 mb-5 w-full border border-slate-300 px-2 py-2 rounded focus-within:outline-blue-300'
-          />
-          {/* Email */}
-          <label className=' font-medium text-slate-700'>Email</label>
-          <input
-            type='text'
-            id='email'
-            name='email'
-            value={data.email}
-            onChange={handleOnchange}
-            placeholder='Email'
-            className='mt-1 mb-5 w-full border border-slate-300 px-2 py-2 rounded focus-within:outline-blue-300'
-          />
-          {/* Password */}
-          <label className=' font-medium text-slate-700'>Password</label>
-          <div className='relative'>
-          <input
-            type={ showPassword ? 'text':'password'} 
-            id='password'
-            name='password'
-            value={data.password}
-            onChange={handleOnchange}
-            placeholder='Password'
-            className='mt-1 mb-5 w-full border border-slate-300 px-2 py-2 rounded focus-within:outline-blue-300'
-          />
-          <span className='flex text-xl cursor-pointer absolute top-4 right-2' onClick={handleShowPassword}>{showPassword ? <BiShow/> : <BiHide/>}</span>
-          </div>
-          
-          {/* Confrim password */}
-          <label className=' font-medium text-slate-700'>Confirm Password</label>
-          <div className='relative'>
-          <input
-            type={ showConfirmPassword ? 'text':'password'} 
-            id='confirmPassword'
-            name='confirmPassword'
-            value={data.confirmPassword}
-            onChange={handleOnchange}
-            placeholder='Password'
-            className='mt-1 mb-5 w-full border border-slate-300 px-2 py-2 rounded focus-within:outline-blue-300'
-          />
-          <span className='flex text-xl cursor-pointer absolute top-4 right-2' onClick={handleConfirmShowPassword}>{showConfirmPassword ? <BiShow/> : <BiHide/>}</span>
-          </div>
+    setConfirmPassword((prev) => !prev);
+  };
 
-          <button type='submit' className='w-full  m-auto py-3 bg-indigo-500 hover:bg-indigo-600 cursor-pointer text-white text-xl font-medium text-center  rounded-xl mt-5'>Sign Up</button>
-        </form>
-        {/* <div className='flex justify-around items-center'>
+  const backToHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <div className="ml-10 mr-10 flex w-full flex-1">
+        <img
+          src={banner1}
+          alt="Hoi An"
+          className="h-screen w-1/2 object-cover"
+        />
+        <div className="relative ml-10 flex h-screen w-1/2 flex-1 flex-col items-center px-[140px] pt-[25px]">
+          <div
+            onClick={backToHome}
+            className="absolute left-0 h-[50px] w-[50px] cursor-pointer text-[35px] hover:opacity-70"
+          >
+            <FaLongArrowAltLeft />
+          </div>
+          <div className="flex w-full flex-col">
+            <h1 className="font-poppins text-[74px] font-bold text-black">
+              Travel with us
+            </h1>
+            <p className="mt-[-23px] text-[36px] font-light text-black">
+              Join us today
+            </p>
+          </div>
+          <form className="flex w-full flex-col py-3" onSubmit={handleSubmit}>
+            <div className="flex">
+              <div className=" flex-1">
+                <label className=" font-medium text-slate-700">
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  id="first_name"
+                  name="first_name"
+                  placeholder="Name"
+                  value={data.first_name}
+                  onChange={handleOnchange}
+                  className="mb-5 mt-1 h-[59px] w-full rounded-[48px] border border-slate-300 px-[36px] py-[18px] focus-within:outline-blue-300"
+                />
+              </div>
+              <div className="ml-5 flex-1">
+                <label className=" font-medium text-slate-700">Last Name</label>
+                <input
+                  type="text"
+                  id="last_name"
+                  name="last_name"
+                  placeholder="Name"
+                  value={data.last_name}
+                  onChange={handleOnchange}
+                  className="mb-5 mt-1 h-[59px] w-full rounded-[48px] border border-slate-300 px-[36px] py-[18px] focus-within:outline-blue-300"
+                />
+              </div>
+            </div>
+            {/* User name */}
+            <label className=" font-medium text-slate-700">User Name</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={data.username}
+              onChange={handleOnchange}
+              placeholder="User name"
+              className="mb-5 mt-1 h-[59px] w-full rounded-[48px] border border-slate-300 px-[36px] py-[18px] focus-within:outline-blue-300"
+            />
+            {/* Email */}
+            <label className=" font-medium text-slate-700">Email</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={data.email}
+              onChange={handleOnchange}
+              placeholder="Email"
+              className="mb-5 mt-1 h-[59px] w-full rounded-[48px] border border-slate-300 px-[36px] py-[18px] focus-within:outline-blue-300"
+            />
+            {/* Password */}
+            <label className=" font-medium text-slate-700">Password</label>
+            <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                name="password"
+                value={data.password}
+                onChange={handleOnchange}
+                placeholder="Password"
+                className="mb-5 mt-1 h-[59px] w-full rounded-[48px] border border-slate-300 px-[36px] py-[18px] focus-within:outline-blue-300"
+              />
+              <span
+                className="absolute right-[35px] top-[22px] flex cursor-pointer text-[25px]"
+                onClick={handleShowPassword}
+              >
+                {showPassword ? <BiShow /> : <BiHide />}
+              </span>
+            </div>
+
+            {/* Confrim password */}
+            <label className=" font-medium text-slate-700">
+              Confirm Password
+            </label>
+            <div className="relative">
+              <input
+                type={showConfirmPassword ? "text" : "password"}
+                id="confirmPassword"
+                name="confirmPassword"
+                value={data.confirmPassword}
+                onChange={handleOnchange}
+                placeholder="Password"
+                className="mb-5 mt-1 h-[59px] w-full rounded-[48px] border border-slate-300 px-[36px] py-[18px] focus-within:outline-blue-300"
+              />
+              <span
+                className="absolute right-[35px] top-[22px] flex cursor-pointer text-[25px]"
+                onClick={handleConfirmShowPassword}
+              >
+                {showConfirmPassword ? <BiShow /> : <BiHide />}
+              </span>
+            </div>
+
+            <button
+              type="submit"
+              className="mt-[20px] h-[59px] w-full cursor-pointer rounded-[48px] bg-black text-center text-[24px] font-[700] text-white hover:opacity-[70%]"
+            >
+              Sign Up
+            </button>
+          </form>
+          {/* <div className='flex justify-around items-center'>
           <span className=' h-px bg-slate-900 w-72'></span> <span className=' mx-2'>or</span> <span className=' h-px bg-slate-900 w-72'></span>
         </div>
         <div className='mt-5 w-full flex justify-around'>
           <button className='border border-slate-700 hover:border-slate-900 hover:bg-slate-100 py-2 px-20 rounded-lg focus:outline-none '><FcGoogle className=' text-4xl' /></button>
           <button className='border border-slate-700 hover:border-slate-900 hover:bg-slate-100 py-2 px-20 rounded-lg focus:outline-none '><BsFacebook className=' text-4xl' /></button>
         </div> */}
-        <p className='text-left text-lg mt-2'>Already have account ? <Link to={'/login'} className='text-indigo-500 underline text-lg'>Login</Link></p>
+          <p className="mt-2 text-left text-lg">
+            Already have account ?{" "}
+            <Link to={"/login"} className="text-lg text-indigo-500 underline">
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
+      <div className="absolute bottom-0 flex w-full justify-around bg-white py-[12px] text-[#666]">
+        <p className="mx-[20px]">About</p>
+        <p className="mx-[20px]">Help Center</p>
+        <p className="mx-[20px]">Terms of Service</p>
+        <p className="mx-[20px]">Privacy Policy</p>
+        <p className="mx-[20px]">Cookie Policy</p>
+        <p className="mx-[20px]">Accessibility</p>
+        <p className="mx-[20px]">Careers</p>
+        <p className="mx-[20px]">Marketing</p>
+        <p className="mx-[20px]">Developers</p>
+        <p className="mx-[20px]">Settings</p>
+        <p className="mx-[20px]">@Capstone2</p>
       </div>
     </div>
-  )
-}
+  );
+};
